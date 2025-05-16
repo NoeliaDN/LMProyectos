22f59de9d8855c0962617e26e7b9bcb3b58799f5
Noelia Delgado Noguerales <noelia.delgado@tajamar365.com>
2025-05-16 11:58:31 +0200

    empezando con JavaScript

---
+++ Práctica4. JavaScript/Introduccion.Variables/script.js
@@ -0,0 +1,11 @@
+console.log("Hola ke ase");
+alert("Hola ke ase");
+
+
+// Variables en JavaScript (ámbitos):
+
+var num = 1; //aámbito global
+let nombre = "Juan"; //ámbito local
+const apellido = "Pérez"; //se espera que no cambie
+
+console.log(num, nombre, apellido);
